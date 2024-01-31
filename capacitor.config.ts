a824515cec75ec6ced7d89f60aef5feb49c3258c
GoldenBookDev <goldenbook.dev@gmail.com>
2024-01-31 11:11:20 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'goldenbook-ionic',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
